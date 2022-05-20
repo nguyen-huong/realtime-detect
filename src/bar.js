bf7f892ed46572a9881c1b678d5a6455f861df49
nguyen-huong <mhuong1703@gmail.com>
2022-05-20 01:51:20 -0700

    show fps

---
+++ src/bar.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import { useDispatch } from 'react-redux';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+const barApp = () => {
+
+    return (
+        <Fragment>
+            <Typography variant="h4"> Notistack redux example</Typography>
+        </Fragment>
+    );
+};
+
+export default barApp;
